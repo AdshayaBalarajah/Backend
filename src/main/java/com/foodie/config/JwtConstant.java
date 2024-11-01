834082335b204ee4ba7fd132069797657f27dcd2
AdshayaBalarajah <bala.adshaya28@gmail.com>
2024-11-01 23:12:05 +0530

    Initial commit of backend

---
+++ src/main/java/com/foodie/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.foodie.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "niwfiwqfifeegjrbgjrngeingeojojwfowjfejjfekjfeifejfnnjaujsdls";
+    public static final String JWT_HEADER = "Authorization";
+}
